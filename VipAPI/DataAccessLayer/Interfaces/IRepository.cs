a8dce854acf0b8e88beab762feccea7dc74a7217
Teodora Gavrilovic <teodora.gavrilovic98@gmail.com>
2022-08-09 18:26:52 +0200

    Repository Interfaces

---
+++ VipAPI/DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IRepository<TEnthity> where TEnthity : class
+    {
+        public void Add(TEnthity enthity);
+        public void Update(TEnthity enthity);
+        public void Delete(TEnthity enthity);
+        Task<List<TEnthity>> GetAll();
+        Task<TEnthity> FindById(int id);
+    }
+}
